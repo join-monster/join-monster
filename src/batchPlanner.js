9d02d3c24fc88946dcbe39b4c49bcfd18c05050a
Andrew Carlson <acarl005@g.ucla.edu>
2017-02-17 11:38:06 -0800

    parallelize batches that arent dependant

--- src/batchPlanner.js
+++ src/batchPlanner.js
@@ -16,58 +16,23 @@ export default async function nextBatch(sqlAST, data, dbCall, context, options) 
     return 
   }
 
+
   // loop through all the child fields that are tables
-  for (let childAST of sqlAST.children) {
+  return Promise.all(sqlAST.children.map(async childAST => {
     if (childAST.type === 'table') {
       const fieldName = childAST.fieldName
       // see if any begin a new batch
-      if (childAST.sqlBatch) {
-        childAST.children.push(childAST.sqlBatch.thisKey)
-        const thisField = childAST.sqlBatch.thisKey.fieldName
-        const parentField = childAST.sqlBatch.parentKey.fieldName
-        if (Array.isArray(data)) {
-          const batchScope = uniq(data.map(obj => maybeQuote(obj[parentField])))
-          const { sql, shapeDefinition } = await compileSqlAST(childAST, context, { ...options, batchScope } )
-          let newData = await handleUserDbCall(dbCall, sql, wrap(shapeDefinition))
-          newData = groupBy(newData, thisField)
-          if (childAST.paginate) {
-            forIn(newData, (group, key, obj) => {
-              obj[key] = arrToConnection(group, childAST)
-            })
-          }
-          if (childAST.grabMany) {
-            for (let obj of data) {
-              obj[fieldName] = newData[obj[parentField]] || []
-            }
-          } else {
-            for (let obj of data) {
-              obj[fieldName] = arrToConnection(newData[obj[parentField]][0], childAST)
-            }
-          }
-          const nextLevelData = flatMap(data, obj => obj[fieldName])
-          await nextBatch(childAST, nextLevelData, dbCall, context, options)
-        } else {
-          const batchScope = [ data[parentField] ]
-          const { sql, shapeDefinition } = await compileSqlAST(childAST, context, { ...options, batchScope } )
-          let newData = await handleUserDbCall(dbCall, sql, wrap(shapeDefinition))
-          newData = groupBy(newData, thisField)
-          if (childAST.paginate){
-            const targets = newData[data[parentField]]
-            data[fieldName] = arrToConnection(targets, childAST)
-          } else {
-            if (childAST.grabMany) {
-              data[fieldName] = newData[data[parentField]] || []
-            } else {
-              const targets = newData[data[parentField]] || []
-              data[fieldName] = targets[0]
-            }
-          }
-          await nextBatch(childAST, data[fieldName], dbCall, context, options)
+      if (childAST.sqlBatch || childAST.junctionBatch) {
+        let thisField, parentField
+        if (childAST.sqlBatch) {
+          childAST.children.push(childAST.sqlBatch.thisKey)
+          thisField = childAST.sqlBatch.thisKey.fieldName
+          parentField = childAST.sqlBatch.parentKey.fieldName
+        } else if (childAST.junctionBatch) {
+          childAST.children.push(childAST.junctionBatch.thisKey)
+          thisField = childAST.junctionBatch.thisKey.fieldName
+          parentField = childAST.junctionBatch.parentKey.fieldName
         }
-      } else if (childAST.junctionBatch) {
-        childAST.children.push(childAST.junctionBatch.thisKey)
-        const thisField = childAST.junctionBatch.thisKey.fieldName
-        const parentField = childAST.junctionBatch.parentKey.fieldName
         if (Array.isArray(data)) {
           const batchScope = uniq(data.map(obj => maybeQuote(obj[parentField])))
           const { sql, shapeDefinition } = await compileSqlAST(childAST, context, { ...options, batchScope } )
@@ -88,7 +53,7 @@ export default async function nextBatch(sqlAST, data, dbCall, context, options) 
             }
           }
           const nextLevelData = flatMap(data, obj => obj[fieldName])
-          await nextBatch(childAST, nextLevelData, dbCall, context, options)
+          return nextBatch(childAST, nextLevelData, dbCall, context, options)
         } else {
           const batchScope = [ data[parentField] ]
           const { sql, shapeDefinition } = await compileSqlAST(childAST, context, { ...options, batchScope } )
@@ -105,18 +70,18 @@ export default async function nextBatch(sqlAST, data, dbCall, context, options) 
               data[fieldName] = targets[0]
             }
           }
-          await nextBatch(childAST, data[fieldName], dbCall, context, options)
+          return nextBatch(childAST, data[fieldName], dbCall, context, options)
         }
       } else {
         if (Array.isArray(data)) {
           const nextLevelData = flatMap(data, obj => obj[fieldName])
-          await nextBatch(childAST, nextLevelData, dbCall, context, options)
+          return nextBatch(childAST, nextLevelData, dbCall, context, options)
         } else {
           if (data) {
-            await nextBatch(childAST, data[fieldName], dbCall, context, options)
+            return nextBatch(childAST, data[fieldName], dbCall, context, options)
           }
         }
       }
     }
-  }
+  }))
 }
