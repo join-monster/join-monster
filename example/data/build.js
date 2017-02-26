9ce9d9ca020addc212093fb06feb1b520b0bd1df
Andrew Carlson <acarl005@g.ucla.edu>
2017-02-26 01:01:37 -0800

    put data importing into knex to make it more portable

--- example/data/build.js
+++ example/data/build.js
@@ -12,18 +12,13 @@ assert(process.env.PG_URL, 'Environment variable PG_URL must be defined, e.g. "p
   console.log('building mysql')
   process.env.DB = 'MYSQL'
   await require('./setup/test1')()
+  await require('./setup/test2')()
 
   console.log('building postgres')
   process.env.DB = 'PG'
   await require('./setup/test1')()
+  await require('./setup/test2')()
   await require('./setup/demo')()
-
-  console.log('restoring dump...')
-
-  const out = execSync(`psql ${process.env.PG_URL}test2 < ${__dirname}/setup/test2-dump.sql`)
-  if (out.toString()) {
-    console.log(out.toString())
-  }
 })()
 .catch(err => {
   console.error(err)
