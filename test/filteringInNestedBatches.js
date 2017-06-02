a93a77c2f09daaeaaaa2d041b304e7ba897445b9
Marek Polak <marek.polak@rtsmunity.com>
2017-06-02 08:42:52 +0200

    Remove not matched items from result while filtering in nested batch

---
+++ test/filteringInNestedBatches.js
@@ -0,0 +1,196 @@
+import test from 'ava'
+import {graphql} from 'graphql'
+import schemaBasic from '../test-api/schema-basic/index'
+import {partial} from 'lodash'
+
+const run = partial(graphql, schemaBasic)
+
+test('it should get user 1 with comments and particular posts', async t => {
+  const query = `
+    {
+      user(id: 1)
+      {
+        comments
+        {
+          id
+          post 
+          {
+            comments
+            {
+              id
+            }
+          }
+        }
+      }
+    }`
+  const {data, errors} = await run(query)
+  t.is(errors, undefined)
+  const expect = {
+    user: {
+      comments: [{
+        id: 1,
+        post: {
+          comments: [{
+            id: 3
+          },
+          {
+            id: 2
+          }, {
+            id: 1
+          }]
+        }
+      }, {
+        id: 4,
+        post: {
+          comments: [
+            {
+              id: 8
+            },
+            {
+              id: 7
+            },
+            {
+              id: 6
+            },
+            {
+              id: 5
+            },
+            {
+              id: 4
+            }]
+        }
+      }, {
+        id: 6,
+        post: {
+          comments: [
+            {
+              id: 8
+            },
+            {
+              id: 7
+            },
+            {
+              id: 6
+            },
+            {
+              id: 5
+            },
+            {
+              id: 4
+            }]
+        }
+      }, {
+        id: 8,
+        post: {
+          comments: [
+            {
+              id: 8
+            },
+            {
+              id: 7
+            },
+            {
+              id: 6
+            },
+            {
+              id: 5
+            },
+            {
+              id: 4
+            }
+          ]
+        }
+      }
+      ]
+    }
+  }
+
+  t.deepEqual(expect, data)
+})
+
+test('it should get user 1 with comments and particular posts with active comments', async t => {
+  const query = `
+    {
+      user(id: 1)
+      {
+        comments
+        {
+          id
+          post 
+          {
+            comments (active: true)
+            {
+              id
+            }
+          }
+        }
+      }
+    }`
+  const {data, errors} = await run(query)
+  t.is(errors, undefined)
+  const expect = {
+    user: {
+      comments: [
+        {
+          id: 1,
+          post: {
+            comments: [
+              {
+                id: 3
+              }, {
+                id: 1
+              }
+            ]
+          }
+        }, {
+          id: 4,
+          post: {
+            comments: [
+              {
+                id: 8
+              }, {
+                id: 6
+              }, {
+                id: 5
+              }, {
+                id: 4
+              }
+            ]
+          }
+        }, {
+          id: 6,
+          post: {
+            comments: [
+              {
+                id: 8
+              }, {
+                id: 6
+              }, {
+                id: 5
+              }, {
+                id: 4
+              }
+            ]
+          }
+        }, {
+          id: 8,
+          post: {
+            comments: [
+              {
+                id: 8
+              }, {
+                id: 6
+              }, {
+                id: 5
+              }, {
+                id: 4
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+
+  t.deepEqual(expect, data)
+})
