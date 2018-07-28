306a83c207aed9639cb45a2601c3e3d8ad07b3f6
Wessel van der Veen <w.vanderveen@thesio.eu>
2018-07-28 11:27:45 +0200

    reverted unnecessary changes.

--- src/index.d.ts
+++ src/index.d.ts
@@ -75,16 +75,16 @@ export interface GraphQLInterfaceTypeConfig<TSource, TContext> {
 
 // JoinMonster lib interface
 
-interface DialectModule {name: string}
+interface DialectModule { name: string }
 
 type Dialect = 'pg' | 'oracle' | 'mariadb' | 'mysql' | 'sqlite3'
 type JoinMonsterOptions = { minify?: boolean, dialect?: Dialect, dialectModule?: DialectModule }
 
 type Rows = any
-type DbCallCallback = (sql: string, done: (err?: any, rows?: Rows) => void) => void
+type DbCallCallback = (sql:string, done: (err?: any, rows?: Rows) => void) => void
 type DbCallPromise = (sql: string) => Promise<Rows>
 type DbCall = DbCallCallback | DbCallPromise
 
-declare function joinMonster(resolveInfo: any, context: any, dbCall: DbCallCallback | DbCallPromise, options?: JoinMonsterOptions): Promise<any>
+declare function joinMonster(resolveInfo: any, context: any, dbCall: DbCallCallback | DbCallPromise, options?: JoinMonsterOptions) : Promise<any>
 
 export default joinMonster
