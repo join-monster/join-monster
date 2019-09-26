bf3f14161e3fe07efc7245e7a92797cfea5eeba0
Alessandro Ferrari <a.ferrari@gruppofood.com>
2019-09-26 11:32:35 +0200

    Update graphql to 14.5.7

--- src/query-ast-to-sql-ast/index.js
+++ src/query-ast-to-sql-ast/index.js
@@ -147,7 +147,7 @@ export function populateASTNode(
   // the actual type might be wrapped in a GraphQLNonNull type
   let gqlType = stripNonNullType(field.type)
 
-  sqlASTNode.args = getArgumentValues(field, queryASTNode, this.variableValues)
+  sqlASTNode.args = field.args ? getArgumentValues(field, queryASTNode, this.variableValues) : {}
 
   // if list then mark flag true & get the type inside the GraphQLList container type
   if (gqlType.constructor.name === 'GraphQLList') {
