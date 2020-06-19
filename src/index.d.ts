81fcac21011e0e7b2f476e19d996763617d9687a
Harry Brundage <harry.brundage@gmail.com>
2020-06-19 14:41:54 -0400

    Update TypeScript types to export strongly typed extension interfaces
    After https://github.com/graphql/graphql-js/pull/2465 , we can now use TypeScript declaration merging to augment the graphql-types nice and cleanly. Woop woop!

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,109 +3,118 @@ export type Maybe<T> = null | undefined | T
 
 // Extend graphql objects and fields
 
-declare module 'graphql/type/definition' {
-  type SqlJoin<TContext, TArgs> = (
-    table1: string,
-    table2: string,
+export type SqlJoin<TContext, TArgs> = (
+  table1: string,
+  table2: string,
+  args: TArgs,
+  context: TContext,
+  sqlASTNode: any
+) => string
+export type Where<TContext, TArgs> = (
+  usersTable: string,
+  args: TArgs,
+  context: TContext,
+  sqlASTNode: any
+) => string | void
+export type Order = 'ASC' | 'asc' | 'DESC' | 'desc'
+export type OrderBy = string | { [key: string]: Order }
+export type ThunkWithArgsCtx<T, TContext, TArgs> =
+  | ((args: TArgs, context: TContext) => T)
+  | T
+
+export interface ObjectTypeExtension<TSource, TContext> {
+  alwaysFetch?: string
+  sqlTable?: ThunkWithArgsCtx<string, any, TContext>
+  uniqueKey?: string | string[]
+}
+
+export interface FieldConfigExtension<TSource, TContext, TArgs> {
+  ignoreAll?: boolean
+  ignoreTable?: boolean
+  junction?: {
+    include?: ThunkWithArgsCtx<
+      {
+        sqlColumn?: string
+        sqlExpr?: string
+        sqlDeps?: string | string[]
+      },
+      TContext,
+      TArgs
+    >
+    orderBy?: ThunkWithArgsCtx<OrderBy, TContext, TArgs>
+    sortKey?: ThunkWithArgsCtx<
+      {
+        order: Order
+        key: string | string[]
+      },
+      TContext,
+      TArgs
+    >
+    sqlBatch?: {
+      thisKey: string
+      parentKey: string
+      sqlJoin: SqlJoin<TContext, TArgs>
+    }
+    sqlJoins?: [SqlJoin<TContext, TArgs>, SqlJoin<TContext, TArgs>]
+    sqlTable: ThunkWithArgsCtx<string, TContext, TArgs>
+    uniqueKey?: string | string[]
+    where?: Where<any, TArgs>
+  }
+  limit?: ThunkWithArgsCtx<number, any, TContext>
+  orderBy?: ThunkWithArgsCtx<OrderBy, TContext, TArgs>
+  sortKey?: ThunkWithArgsCtx<
+    {
+      order: Order
+      key: string | string[]
+    },
+    TContext,
+    TArgs
+  >
+  sqlBatch?: {
+    thisKey: string
+    parentKey: string
+  }
+  sqlColumn?: string
+  sqlDeps?: string[]
+  sqlExpr?: (
+    table: string,
     args: TArgs,
     context: TContext,
     sqlASTNode: any
   ) => string
-  type Where<TContext, TArgs> = (
-    usersTable: string,
-    args: TArgs,
-    context: TContext,
-    sqlASTNode: any
-  ) => string | void
-  type Order = 'ASC' | 'asc' | 'DESC' | 'desc'
-  type OrderBy = string | { [key: string]: Order }
-  type ThunkWithArgsCtx<T, TContext, TArgs> =
-    | ((args: TArgs, context: TContext) => T)
-    | T
-
-  export interface GraphQLObjectTypeConfig<TSource, TContext> {
-    extensions?: Maybe<Readonly<Record<string, any>>> & {
-      alwaysFetch?: string
-      sqlTable?: ThunkWithArgsCtx<string, any, TContext>
-      uniqueKey?: string | string[]
-    }
-  }
-
-  export interface GraphQLFieldConfig<TSource, TContext, TArgs> {
-    extensions?: Maybe<Readonly<Record<string, any>>> & {
-      ignoreAll?: boolean
-      ignoreTable?: boolean
-      junction?: {
-        include?: ThunkWithArgsCtx<
-          {
-            sqlColumn?: string
-            sqlExpr?: string
-            sqlDeps?: string | string[]
-          },
-          TContext,
-          TArgs
-        >
-        orderBy?: ThunkWithArgsCtx<OrderBy, TContext, TArgs>
-        sortKey?: ThunkWithArgsCtx<
-          {
-            order: Order
-            key: string | string[]
-          },
-          TContext,
-          TArgs
-        >
-        sqlBatch?: {
-          thisKey: string
-          parentKey: string
-          sqlJoin: SqlJoin<TContext, TArgs>
-        }
-        sqlJoins?: [SqlJoin<TContext, TArgs>, SqlJoin<TContext, TArgs>]
-        sqlTable: ThunkWithArgsCtx<string, TContext, TArgs>
-        uniqueKey?: string | string[]
-        where?: Where<TContext, TArgs>
-      }
-      limit?: ThunkWithArgsCtx<number, any, TContext>
-      orderBy?: ThunkWithArgsCtx<OrderBy, TContext, TArgs>
-      sortKey?: ThunkWithArgsCtx<
-        {
-          order: Order
-          key: string | string[]
-        },
-        TContext,
-        TArgs
-      >
-      sqlBatch?: {
-        thisKey: string
-        parentKey: string
-      }
-      sqlColumn?: string
-      sqlDeps?: string[]
-      sqlExpr?: (
-        table: string,
-        args: TArgs,
-        context: TContext,
-        sqlASTNode: any
-      ) => string
-      sqlJoin?: SqlJoin<TContext, TArgs>
-      sqlPaginate?: boolean
-      where?: Where<TContext, TArgs>
-    }
-  }
+  sqlJoin?: SqlJoin<TContext, TArgs>
+  sqlPaginate?: boolean
+  where?: Where<TContext, TArgs>
 }
 
-export interface GraphQLUnionTypeConfig<TSource, TContext> {
-  extensions?: Maybe<Readonly<Record<string, any>>> & {
-    sqlTable?: string
-    uniqueKey?: string | string[]
-    alwaysFetch?: string
-  }
+export interface UnionTypeExtension {
+  sqlTable?: string
+  uniqueKey?: string | string[]
+  alwaysFetch?: string
 }
 
-export interface GraphQLInterfaceTypeConfig<TSource, TContext> {
-  extensions: Maybe<Readonly<Record<string, any>>> & {
-    sqlTable?: string
-    uniqueKey?: string | string[]
-    alwaysFetch?: string
+export interface InterfaceTypeExtension {
+  sqlTable?: string
+  uniqueKey?: string | string[]
+  alwaysFetch?: string
+}
+
+declare module 'graphql' {
+  interface GraphQLObjectTypeExtensions<TSource = any, TContext = any> {
+    joinMonster?: ObjectTypeExtension<TSource, TContext>
+  }
+  interface GraphQLFieldExtensions<
+    TSource,
+    TContext,
+    TArgs = { [argName: string]: any }
+  > {
+    joinMonster?: FieldConfigExtension<TSource, TContext, TArgs>
+  }
+  interface GraphQLUnionTypeExtensions {
+    joinMonster?: UnionTypeExtension
+  }
+  interface GraphQLInterfaceTypeExtensions {
+    joinMonster?: InterfaceTypeExtension
   }
 }
 
