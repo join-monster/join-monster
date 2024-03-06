3525939f84928f81c4d5bb3bcb3504a8e8bb8df9
Graham Allan <graham.allan@vendr.com>
2024-03-06 11:36:50 +0000

    Be more careful before invoking resolveType()
     - make the assumption that resolveType can only be used if set up to derive the type only from the resolved value, synchronously
     - check that it's a function that only accepts the value argument of resolveType
     - we do not have the context or info object from the request anywhere nearby to be able to invoke resolveType correctly (see https://github.com/graphql/graphql-js/blob/9c90a23dd430ba7b9db3d566b084e9f66aded346/src/type/definition.ts#L863)
     - if any of the assumptions do not hold, fall back to old behaviour of taking first matching data

--- src/resolve-unions.js
+++ src/resolve-unions.js
@@ -61,11 +61,68 @@ export default function resolveUnions(data, sqlAST) {
   }
 }
 
+/**
+ * Uses the resolveType() function to choose which data is selected for a given field.
+ *
+ * A query that selects different fields from a union type can result in having data from more than one member of the
+ * union.
+ *
+ * For example:
+ *   type Post { author: User }
+ *   type Comment { author: user }
+ *
+ *   union WrittenMaterial = Post | Comment
+ *
+ *   query {
+ *       writtenMaterial {
+ *           ... on Comment {
+ *             author {
+ *               capitalizedLastName # <-- First "disjoint" field selection from author type
+ *             }
+ *           }
+ *           ... on Post {
+ *             author {
+ *               email # <-- Second "disjoint" field selection from author type
+ *             }
+ *           }
+ *       }
+ *   }
+ *
+ *   The data returned from this query, when a Post and a Comment have the same author, can look like this FOR ANY GIVEN
+ *   comment OR post:
+ *   data: {
+ *    "author@Comment": {"id": 1, "capitalizedLastName": "CARLSON"},
+ *    "author@Post": {"id": 1, "email": "andrew@stem.is"}
+ *   }
+ *
+ *   Where any particular result is a Comment, we need to pick the Comment-shaped data, and not the Post-shaped data, and vice-versa.
+ *
+ *   This function calls resolveType() on the schema for the given data in order to pick which key to use for the requested field.
+ *
+ *   Where it is not possible to use resolveType() to choose, the first-encountered key will be used as the field.
+ *
+ *   The resolveType function must be a function that takes 0 or 1 arguments and returns a string. It cannot accept the
+ *   context or info objects, or return a Promise.
+ *
+ * @param obj
+ * @param childASTsql
+ * @param typeName
+ * @param qualifiedName
+ * @param requestedFieldName
+ */
 const disambiguateQualifiedTypeFields = (obj, childASTsql, typeName, qualifiedName, requestedFieldName) => {
-  const discriminatorTypeName = childASTsql.defferedFrom.resolveType ? childASTsql.defferedFrom.resolveType(obj) : null
   const qualifiedValue = obj[qualifiedName]
   delete obj[qualifiedName]
-  if (typeName !== discriminatorTypeName) {
+
+  const resolveType = childASTsql.defferedFrom?.resolveType
+  const resolveTypeFn = typeof resolveType === 'function' && resolveType.length < 2 ? resolveType : null
+  const resolveTypeResult = resolveTypeFn ? resolveTypeFn(obj) : null
+  const discriminatorTypeName = typeof resolveTypeResult === 'string' ? resolveTypeResult : null
+
+  const fieldTypeMatchesResolvedType = discriminatorTypeName && typeName === discriminatorTypeName
+
+  if (!fieldTypeMatchesResolvedType) {
+    // Remove the field@TypeName from obj without replacing it
     return
   }
 
