a5395c589d727f69f29ca36c19aea227f659f1b0
Nico Gallinal <nicoabie@gmail.com>
2024-05-04 15:14:20 -0300

    fix multiple disjoint fields reaquest for union type

--- test/union.js
+++ test/union.js
@@ -208,7 +208,7 @@ test('it should return single disjoint field request for union type', async t =>
   t.deepEqual(expect, data)
 })
 
-test.skip('it should return multiple disjoint fields requests for union type', async t => {
+test('it should return multiple disjoint fields requests for union type', async t => {
   const source = `
     {
       user(id: 1) {
