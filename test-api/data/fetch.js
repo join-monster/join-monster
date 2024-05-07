bb2eedd20cc9c0572eba0b662399be3b264afaca
Nico Gallinal <nicoabie@gmail.com>
2024-05-07 12:50:57 -0300

    use latest graphiql with foorter for sql

--- test-api/data/fetch.js
+++ test-api/data/fetch.js
@@ -2,7 +2,7 @@ module.exports = function dbCall(sql, knex, context) {
   if (context?.res) {
     context.res.set(
       'X-SQL-Preview',
-      sql.replace(/%/g, '%25').replace(/\n/g, '%0A')
+      btoa(sql)
     )
   }
   return knex.raw(sql).then(result => {
--- test-api/server.js
+++ test-api/server.js
@@ -1,38 +1,24 @@
 
 import path from 'path'
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import { createHandler } from 'graphql-http/lib/use/express'
 
-// module we created that lets you serve a custom build of GraphiQL
-import graphiql from 'express-custom-graphiql'
-
 import schemaBasic from './schema-basic/index'
 import schemaRelay from './schema-paginated/index'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json())
 app.use(cors())
 
-app.get(
-  '/graphql',
-  graphiql({
-    css: '/graphiql.css',
-    js: '/graphiql.js'
-  })
-)
+app.get('/graphql', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
 
-app.get(
-  '/graphql-relay',
-  graphiql({
-    url: '/graphql-relay',
-    css: '/graphiql.css',
-    js: '/graphiql.js'
-  })
-)
+app.get('/graphql-relay', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
 
 app.post(
   '/graphql',
@@ -56,9 +42,6 @@ app.post(
     }
   })
 )
-
-// serve the custom build of GraphiQL
-app.use(express.static(path.join(__dirname, '../node_modules/graphsiql')))
 
 app.listen(3000, () =>
   console.log(
