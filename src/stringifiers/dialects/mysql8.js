ac7c66e9cd5e5bd5fb86cf5ab64aeb4dcf493967
Nico Gallinal <nicoabie@gmail.com>
2024-06-02 17:26:47 -0300

    limit for mysql and mysql8

--- src/stringifiers/dialects/mysql8.js
+++ src/stringifiers/dialects/mysql8.js
@@ -1,3 +1,8 @@
+import {
+  interpretForOffsetPaging,
+  offsetPagingSelect
+} from '../shared'
+
 function quote(str) {
   return `\`${str}\``
 }
@@ -12,5 +17,28 @@ module.exports = {
   compositeKey(parent, keys) {
     keys = keys.map(key => `${quote(parent)}.${quote(key)}`)
     return `CONCAT(${keys.join(', ')})`
-  }
+  },
+
+  handlePaginationAtRoot: async function(parent, node, context, tables) {
+    const pagingWhereConditions = []
+    
+    if (node.orderBy) {
+      const { limit, offset, order } = interpretForOffsetPaging(node, { name: 'mysql8' })
+      if (node.where) {
+        pagingWhereConditions.push(
+          await node.where(`"${node.as}"`, node.args || {}, context, node)
+        )
+      }
+      tables.push(
+        offsetPagingSelect(
+          node.name,
+          pagingWhereConditions,
+          order,
+          limit,
+          offset,
+          node.as
+        )
+      )
+    }
+  },
 }
